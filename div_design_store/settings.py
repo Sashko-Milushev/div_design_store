f16c3bd2a8106989973b66c76f60fec05f4f9350
Sashko Milushev <milushev.sashko@gmail.com>
2023-02-12 16:04:41 +0200

    Add path in urls Add app in settings Add urls file in accounts

--- div_design_store/settings.py
+++ div_design_store/settings.py
@@ -21,12 +21,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # build-in apps
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # project apps
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
